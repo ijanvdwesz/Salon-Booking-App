fa1192b8c4e0fa7a8120ed19e8591340b760e27a
Ijan <ijanvdwestz@gmail.com>
2025-07-26 18:11:51 +0200

    MapId-Update

--- Front-end/src/components/Map.jsx
+++ Front-end/src/components/Map.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
+// Declare static values outside the component to avoid re-creating on each render
 const containerStyle = {
   width: '100%',
   height: '400px'
 };
 
 const center = {
-  lat: -26.2041, // Johannesburg (example)
+  lat: -26.2041, // Johannesburg
   lng: 28.0473
 };
 
+const MAP_ID = 'b07920fc03afa33efe62f2a9';
+const LIBRARIES = ['marker']; // keep static
+
 function MapComponent() {
   const handleOnLoad = (map) => {
-    if (window.google && window.google.maps && window.google.maps.marker) {
-      // Add AdvancedMarkerElement instead of Marker
+    if (
+      window.google &&
+      window.google.maps &&
+      window.google.maps.marker &&
+      window.google.maps.marker.AdvancedMarkerElement
+    ) {
       new window.google.maps.marker.AdvancedMarkerElement({
         map,
         position: center,
         title: 'Glamour Beauty & Spa'
       });
     } else {
-      console.warn("AdvancedMarkerElement API not available. Check that 'libraries=marker' is loaded in your script URL.");
+      console.warn("AdvancedMarkerElement API not available. Make sure 'libraries=marker' is loaded.");
     }
   };
 
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyAosaqSJm5RVYOUSTclJpE0qT4GNSzsXOU"
-      libraries={['marker']}
+      libraries={LIBRARIES}
+      loadingStrategy="async" // recommended
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={15}
         onLoad={handleOnLoad}
+        options={{ mapId: MAP_ID }} // attach your Map ID here
       >
-        {/* Removed <Marker /> */}
+        {/* Marker is added via AdvancedMarkerElement in onLoad */}
       </GoogleMap>
     </LoadScript>
   );
